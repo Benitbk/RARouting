0b44e55926c5fe90ae34aaddc9ae303df77961f6
Guy Aloni <guy.aloni1@gmail.com>
2016-06-06 16:45:41 +0300

    New policy - AgentMaximalCostPolicy The next agent is the one that right now has the most expensive cost

---
+++ RARouting/src/seriesParallelGraph/policies/AgentMaximalCostPolicy.java
@@ -0,0 +1,50 @@
+package seriesParallelGraph.policies;
+
+import seriesParallelGraph.agent.Agent;
+import seriesParallelGraph.game.GameState;
+import seriesParallelGraph.graph.Route;
+
+import java.util.List;
+
+/**
+ * Created by Guy Aloni on 06/06/2016.
+ */
+public class AgentMaximalCostPolicy extends AgentPolicy {
+
+    public AgentMaximalCostPolicy(GameState gameState) {
+        super(gameState);
+    }
+
+    @Override
+    public Agent getNextAgent() {
+        List<Agent> agents = this.gameState.game.agents;
+
+        double maxCost = -1;
+        int nextAgentToPlay=0;
+
+        for (int i=0; i<agents.size();i++)
+        {
+            Agent agent = agents.get(i);
+
+            Route improvedRoute = this.gameState.getImprovedRoute(agent);
+            if (improvedRoute != null)
+            {
+                double currAgentRouteCost = agent.getRoute().cost();
+                if (currAgentRouteCost > maxCost)
+                {
+                    nextAgentToPlay=i;
+                    maxCost = currAgentRouteCost;
+                }
+            }
+        }
+
+        if (maxCost == -1)
+        {
+            return null;
+        }
+        else
+        {
+            return agents.get(nextAgentToPlay);
+        }
+    }
+}
